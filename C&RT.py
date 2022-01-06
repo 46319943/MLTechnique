caa6368d5f278aad296ce3aa3623d855a4631347
46319943 <495384481@qq.com>
2022-01-06 15:26:17 +0800

    preliminary random forest

--- C&RT.py
+++ C&RT.py
@@ -1,6 +1,39 @@
 import numpy as np
 from random import randrange
 
+g_Ein_list = []
+
+
+class RandomForest:
+
+    def __init__(self, T, N_apostrophe):
+        self.T = T
+        self.N_apostrophe = N_apostrophe
+        self.g_list = []
+
+    def fit(self, X, Y):
+        N = X.shape[0]
+
+        for t in range(self.T):
+            sample_mask = np.random.choice(N, self.N_apostrophe)
+            X_sample = X[sample_mask]
+            Y_sample = Y[sample_mask]
+            g = DecisionTree('classification').fit(X_sample, Y_sample)
+            self.g_list.append(g)
+
+            g_Ein_list.append(
+                np.average(Y != g.batch_predict(X))
+            )
+
+        return self
+
+    def __call__(self, X):
+        N = X.shape[0]
+        Y = np.zeros((N, 1))
+        for g in self.g_list:
+            Y += g.batch_predict(X)
+        return Y / N
+
 
 class DecisionTree:
     internal_nodes_count = 0
@@ -27,14 +60,13 @@ class DecisionTree:
     def fit(self, X, Y):
         # check termination criteria
         impurity = gini_impurity(Y)
-        x_unique = np.unique(X)
+        x_unique = np.unique(X, axis=0)
         if impurity == 0 or x_unique.shape[0] == 1:
             self.constant = optimal_constant(Y, self.error)
             return self
 
-        self.internal_nodes_count += 1
-        self.feature_index, self.threshold, D1_mask, D2_mask = decision_stump_branch(X, Y,
-                                                                                                     gini_impurity)
+        DecisionTree.internal_nodes_count += 1
+        self.feature_index, self.threshold, D1_mask, D2_mask = decision_stump_branch(X, Y, gini_impurity)
         self.X1 = X[D1_mask]
         self.X2 = X[D2_mask]
         self.Y1 = Y[D1_mask]
@@ -79,13 +111,15 @@ def decision_stump_branch(X, Y, impurity_function):
     feature_threshold_direction_list = []
 
     feature_num = X.shape[1]
-    N = X.shape[0]
+
     for feature_index in range(feature_num):
         x = X[:, [feature_index]]
         # 对当前单一特征维度去重
-        x_unique = np.unique(x)
+        x_unique = np.unique(x, axis=0)
         # 对去重后的特征排序，要和原始的未排序的x分开
         x_sorted = np.sort(x_unique, axis=0)
+        # 去重后重新计算特征值数量
+        N = x_sorted.shape[0]
         # 获取切割点阈值
         threshold_list = [(x_sorted[x_index, 0] + x_sorted[x_index + 1, 0]) / 2 for x_index in range(N - 1)]
 
@@ -94,6 +128,8 @@ def decision_stump_branch(X, Y, impurity_function):
 
             D1_mask = X[:, [feature_index]] < threshold
             D2_mask = X[:, [feature_index]] > threshold
+            D1_mask = D1_mask.reshape(-1)
+            D2_mask = D2_mask.reshape(-1)
 
             D1_num = np.sum(D1_mask)
             D2_num = np.sum(D2_mask)
@@ -108,6 +144,7 @@ def decision_stump_branch(X, Y, impurity_function):
 
             if min_impurity is None or weighted_impurity < min_impurity:
                 min_impurity = weighted_impurity
+                feature_threshold_direction_list.clear()
                 feature_threshold_direction_list.append((feature_index, threshold, D1_mask, D2_mask))
 
     return feature_threshold_direction_list[randrange(len(feature_threshold_direction_list))]
@@ -131,10 +168,15 @@ def optimal_constant(Y, error):
     :param error:
     :return:
     '''
+    Y = Y.reshape(-1)
     if error == 'regression':
         return np.mean(Y)
     elif error == 'classification':
-        return np.bincount(Y).argmax()
+        # This method only applies to positive integer
+        # return np.bincount(Y).argmax()
+
+        values, counts = np.unique(Y, return_counts=True)
+        return values[np.argmax(counts)]
 
     raise Exception('undefined error type')
 
@@ -148,5 +190,20 @@ if __name__ == '__main__':
     X_test = test_raw[:, :-1]
     Y_test = test_raw[:, [-1]]
 
-    t = DecisionTree('classification').fit(X_train, Y_train)
-    t.batch_predict(X_train)
+    N = X_train.shape[0]
+
+    # t = DecisionTree('classification').fit(X_train, Y_train)
+    #
+    # Y_hat_train = t.batch_predict(X_train)
+    # Y_hat_test = t.batch_predict(X_test)
+    # Ein = np.average(Y_hat_train != Y_train)
+    # Eout = np.average(Y_hat_test != Y_test)
+
+    f = RandomForest(300, N).fit(X_train, Y_train)
+    Y_hat_train = f(X_train)
+    Y_hat_test = f(X_test)
+
+    np.average(np.sign(Y_hat_train) != Y_train)
+    np.average(np.sign(Y_hat_test) != Y_test)
+
+    print()
